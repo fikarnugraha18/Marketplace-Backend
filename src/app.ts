5e8b8ba6a0d6bc9aaaa01cbaa146c337f0d2a2b9
Fikar <fikarnugraha18@gmail.com>
2026-03-11 14:23:14 +0700

    add root route

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,10 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.send("Marketplace API running");
+});
 app.use("/api/products", productRouter)
 app.use("/api/cart", cartRouter)
 
